2946cd9142d08a40a25e14d4e24e44865455163b
chianghomer <chianghomer@gmail.com>
2015-02-12 16:25:39 +0800

    hi

--- app/src/main/java/com/studentproject/stayconnect/DisplayDoctorListActivity.java
+++ app/src/main/java/com/studentproject/stayconnect/DisplayDoctorListActivity.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.webkit.DownloadListener;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
@@ -15,7 +17,6 @@ import android.widget.Button;
 public class DisplayDoctorListActivity extends Activity {
 
     private WebView mWebView;
-    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,65 +25,23 @@ public class DisplayDoctorListActivity extends Activity {
         init();
     }
 
-    private void init(){
+    private void init() {
         mWebView = (WebView) findViewById(R.id.doctorList_webView);
         loadPDF();
     }
 
-    private void loadPDF(){
+    private void loadPDF() {
         mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.getSettings().setSupportZoom(true);
-        mWebView.getSettings().setDomStorageEnabled(true);
-        mWebView.getSettings().setAllowFileAccess(true);
-        mWebView.getSettings().setUseWideViewPort(true);
-        mWebView.getSettings().setBuiltInZoomControls(true);
-        mWebView.requestFocus();
         mWebView.getSettings().setLoadWithOverviewMode(true);
-        String pdfUrl = "http://www.pdf995.com/samples/pdf.pdf";
-        mWebView.loadUrl("http://docs.google.com/gview?embedded=true&url="+pdfUrl);
+        mWebView.getSettings().setUseWideViewPort(true);
+        mWebView.loadUrl("http://docs.google.com/gview?embedded=true&url=http://www.stagecoachbus.com/PdfUploads/Timetable_28768_5.pdf");
 
-        mWebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                super.onPageStarted(view, url, favicon);
-            }
-
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-
-            }
-
-            @Override
-            public void onReceivedError(WebView view, int errorCode,
-                                        String description, String failingUrl) {
-                super.onReceivedError(view, errorCode, description, failingUrl);
-
-            }
-
-        });
-
-        mWebView.setDownloadListener(new DownloadListener() {
-            @Override
-            public void onDownloadStart(String url, String userAgent,
-                                        String contentDisposition, String mimetype,long contentLength) {
-                System.out.println("=========>开始下载 url =" + url);
-                Uri uri = Uri.parse(url);
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
-        });
 
 
     }
 
-
-
-
 }
+
+
+
+
