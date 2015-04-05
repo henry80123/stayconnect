44e428891bc4e618a3d20a9e34dc516d98c4cf5a
chianghomer <chianghomer@gmail.com>
2015-04-05 15:28:44 +0800

    updated 2Apr2015 - move layout up when keyboard pops up at launch page. - add an effect on "Log In" button when is pressed (temp). - add image effect when click on the puzzle (temp).

--- app/src/main/java/com/studentproject/stayconnect/LoadScreenActivity.java
+++ app/src/main/java/com/studentproject/stayconnect/LoadScreenActivity.java
@@ -4,16 +4,22 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.WindowManager;
+import android.view.ViewTreeObserver;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
@@ -22,16 +28,23 @@ public class LoadScreenActivity extends Activity {
 
     private static final String tag = "LoadScreen";
     private ProgressBar spinner;
-    private Button logIn;
     private EditText username;
     private EditText pw;
     private TextView appname;
-
-    private AlertDialog.Builder alertBuilder;  //For success
+    private Button loginBtn;
+    private ImageView logo;
+    private AlertDialog.Builder alertBuilder;  //For successfully
     private AlertDialog.Builder alertBuilder2; //For fail
-    private AlertDialog.Builder alertBuilder3; //For fail to how to register
+    private AlertDialog.Builder alertBuilder3; //For fail and how to register
     private AlertDialog dialog2;//For fail
-    private AlertDialog dialog3;//For fail to how to register
+    private AlertDialog dialog3;//For fail and how to register
+    private int marginTop;
+
+    private boolean isPressed =false;
+    private int i=1;
+
+
+    private RelativeLayout.LayoutParams lp;
 
     private Animation anim_appname;
     //private Animation anim_username;
@@ -40,21 +53,20 @@ public class LoadScreenActivity extends Activity {
     //private AnimationSet animSet;
 
     @Override
-        protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
             //supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.launch_login_layou);
-            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+            setContentView(R.layout.launch_login_layout);
 
-            //
+
             username = (EditText) findViewById(R.id.username_edit);
             pw = (EditText) findViewById(R.id.pw_edit);
-            logIn = (Button) findViewById(R.id.login_btn);
+            loginBtn = (Button) findViewById(R.id.login_btn);
             appname = (TextView)findViewById(R.id.startscreen_text);
 
             username.setVisibility(View.GONE);
             pw.setVisibility(View.GONE);
-            logIn.setVisibility(View.GONE);
+            loginBtn.setVisibility(View.GONE);
 
             alertBuilder = new AlertDialog.Builder(this);
             alertBuilder2 = new AlertDialog.Builder(this);
@@ -65,11 +77,13 @@ public class LoadScreenActivity extends Activity {
             //anim_pw = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.launch_appname);
             //anim_login_btn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.launch_appname);
 
-            //animSet = new AnimationSet(true);
+            //move the screen up when keyboard pops up.
+            lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            logo = (ImageView)findViewById(R.id.startscreen_image);
+            marginTop = getResources().getDimensionPixelSize(R.dimen.margin_top);
+            lp.addRule(RelativeLayout.CENTER_HORIZONTAL,logo.getId());
 
 
-        //spinner = (ProgressBar)findViewById(R.id.progressBar1);
-            //spinner.setVisibility(View.GONE);
 
             final Thread welcomeThread = new Thread() {
 
@@ -101,7 +115,7 @@ public class LoadScreenActivity extends Activity {
                                         appname.setVisibility(View.GONE);
                                         username.setVisibility(View.VISIBLE);
                                         pw.setVisibility(View.VISIBLE);
-                                        logIn.setVisibility(View.VISIBLE);
+                                        loginBtn.setVisibility(View.VISIBLE);
                                     }
 
                                     @Override
@@ -117,6 +131,11 @@ public class LoadScreenActivity extends Activity {
                 }
             };
             welcomeThread.start();
+            buttonEffect(loginBtn);
+            imageEffect();
+            moveScreenUp();
+
+
     }
 
     //for showing the alertdialog when user click the "Log In" button.
@@ -131,12 +150,19 @@ public class LoadScreenActivity extends Activity {
                     startActivity(i);
                 }
             });
-            //For login success
+            //For login successfully
             AlertDialog dialog = alertBuilder.show();
+
+            //Disable the dismiss ability for dialog for login successfully.
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.setCancelable(false);
+            //
+
             TextView messageText = (TextView)dialog.findViewById(android.R.id.message);
             messageText.setGravity(Gravity.CENTER);
             messageText.setTextColor(getResources().getColor(R.color.background_material_dark));
             dialog.show();
+
 
         }else{
 
@@ -175,6 +201,77 @@ public class LoadScreenActivity extends Activity {
             dialog2.show();
 
         }
+
+    }
+
+    public void moveScreenUp(){
+        final View activityRootView = findViewById(R.id.activityRoot);
+        activityRootView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                Rect r = new Rect();
+                activityRootView.getWindowVisibleDisplayFrame(r);
+                int heightDiff = activityRootView.getRootView().getHeight() - r.bottom;
+                if (heightDiff > 100) {
+                    lp.setMargins(0, 0, 0, 0);
+                    logo.setLayoutParams(lp);
+                } else {
+                    lp.setMargins(0, marginTop, 0, 0);
+                    logo.setLayoutParams(lp);
+                }
+            }
+        });
+
+    }
+
+    public static void buttonEffect(View button){
+        button.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        v.getBackground().setColorFilter(0xe02ca491, PorterDuff.Mode.SRC_ATOP);
+                        v.invalidate();
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.getBackground().clearColorFilter();
+                        v.invalidate();
+                        break;
+
+                    case MotionEvent.ACTION_CANCEL:
+                        v.getBackground().clearColorFilter();
+                        v.invalidate();
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    public void imageEffect(){
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(isPressed &&i==1){
+                    logo.setImageResource(R.drawable.puzzle_logo2);
+                    i++;
+                }else if(i==2){
+                    logo.setImageResource(R.drawable.puzzle_logo3);
+                    i++;
+                }else if(i==3){
+                    logo.setImageResource(R.drawable.puzzle_logo4);
+                    i++;
+                }else if(i==4){
+                    logo.setImageResource(R.drawable.puzzle_logo5);
+                    i=1;
+                }
+                isPressed =!isPressed;
+
+            }
+
+        });
 
     }
 
